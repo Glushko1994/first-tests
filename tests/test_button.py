047b4884d23d7a4393510a632ed2ba818798a86a
Геннадий <Glushkovski724@mail.ru>
2026-01-27 19:19:29 +0300

    first action

--- tests/test_button.py
+++ tests/test_button.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
+from selenium.webdriver.common.by import  By
 from selenium.webdriver.chrome.options import Options
 import pytest
+
 
 
 @pytest.fixture()
